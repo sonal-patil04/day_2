159db8f4fed4bb84fe82fcdb473cb97685527f6d
Sonal Patil <sonalpatil9370@gmail.com>
2026-09-11 11:26:21 +0530

    version 1.0.4

--- day2.py
+++ day2.py
@@ -43,15 +43,156 @@
 # print(f"{1234} : last digit is {a%10}")
 
 
-role=str(input("Enter role : "))
-age=int(input("Enter age : "))
-a=(role=="student") and  (age<21)
-print(f"Eligible : {a}")
+# role=str(input("Enter role : "))
+# age=int(input("Enter age : "))
+# a=(role=="student") and  (age<21)
+# print(f"Eligible : {a}")
+
+
+# WAP to swap two variables without third variable using arithmetic operator
+
+# a=10
+# b=20
+# print(f"Before swap : a={a} b={b}")
+# print(f"After swap : a={a+b-a} b={b+a-b}")
 
 
 
-"Hello"
 
-"hey, Good afternoon"
 
-"Welcome to Profile"
+# -----if-else--------- #
+# is_raining=False
+# if is_raining==True:
+#     print("Raining outside")
+# else:
+#     print("Not raining")
+
+
+
+# ------------------------------------------------------------------------------
+# age=int(input("Enter your age: "))
+# if age>=18:
+#     print("Eligible to vote")
+# else:
+#     print("Not eligible to vote")
+
+
+
+# -------------------------------------------------------------------------------------------
+# n=int(input("Enter a number: "))
+# if n==1:
+#     print("Sunday")
+# elif n==2:
+#     print("Monday")
+# elif n==3:
+#     print("Tuesday")
+# elif n==4:
+#     print("Wednesday")
+# elif n==5:
+#     print("Thursday")
+# elif n==6:
+#     print("Friday")
+# elif n==7:
+#     print("Saturday")
+# else:
+#     print("Invalid number")
+
+
+# ------------------------------------------------------------------------------------------------
+# day=int(input("Enter the day number: "))
+# match day:
+#     case 1:
+#         print("Monday")
+#     case 2:
+#         print("Tuesday")
+#     case 3:
+#         print("Wednesday")
+#     case 4:
+#         print("Thursday")
+#     case 5:
+#         print("Friday")
+#     case 6:
+#         print("Saturday")
+#     case 7:
+#         print("Sunday")
+
+
+
+
+
+# --------------------------------------------------------------------------------------------
+
+# n=int(input("Enter a number: "))
+# if n%2==0:
+#     print(f"the number {n} is even")
+# else:
+#     print(f"the number {n} is odd")
+
+
+
+# --------------------------------------------------------------------------------------------
+
+# tic=100
+# disc=tic*10/tic
+# disc_price=100-disc
+
+# age=int(input("Enter your age : "))
+
+# if age<12:
+#     print(f"You have got 10% off on the ticket")
+#     print(f"Your ticket price is {disc_price}")
+# else:
+#     print(f"The ticket price is 100")
+
+
+
+# --------------------------------------------------------------------------------------------
+# marks=int(input("Enter your marks: "))
+# if 100>marks>90:
+#     print("O")
+# elif marks>100:
+#     print("Marks will not be calculated above 100")
+# elif marks>=80:
+#     print("A+")
+# elif marks>=65:
+#     print("B")
+# elif marks>=35:
+#     print("C")
+# elif 0<marks<35:
+#     print("F")
+# else:
+#     print("Invalid marks")
+
+
+# # ------------------------------------------------
+# num=eval(input("enter a number: "))
+# if num>0:
+#     print("The number is positive")
+# elif num==0:
+#     print("0 is neither positive nor negative")
+# else:
+#     print("The number is negative")
+
+
+# ---------------------------------------------------
+# a=int(input("Enter a number: "))
+# b=int(input("Enter a number: "))
+# c=int(input("Enter a number: "))
+
+# if b<a>c:
+#     print("The value of a is greater")
+# elif a<b>c:
+#     print("The value of b is greater")
+# elif a==b==c:
+#     print("All numbers are same")
+# elif a==b>c:
+#     print("the value a & b is greater than c")
+# elif a==c>b:
+#     print("The value of a & c is greater than b")
+# elif b==c>a:
+#     print("The value of b & c is greater than a")
+# else:
+#     print("The value of c is greater")
+
+# --------------------------------------------------------------------------------------
+
