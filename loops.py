7860ba8792f3b8403dc0ec8f47232764684f9fb6
Sonal Patil <sonalpatil9370@gmail.com>
2026-09-15 15:40:49 +0530

    version 1.0.1

---
+++ loops.py
@@ -0,0 +1,96 @@
+# while True:
+#     string=input("Enter a string: ")
+#     print(string)
+
+
+# correct_pass="some_pass"
+# not_found=True
+# while not_found:
+#     string=input("Enter a string: ")
+#     if string == correct_pass:
+#         not_found=False
+#     else:
+#         print("Wrong password try again!")
+# print("Password Matched!")
+
+
+
+# ################################################################
+
+# correct_pass="some_pass"
+# while True:
+#     string=input("Enter a string: ")
+#     if string == correct_pass:
+#         break
+#     else:
+#         print("Wrong password try again!")
+# print("Password Matched!")
+
+# ----------------------------------------------------------------------------------
+
+
+# print count of even numbers
+# i=0
+# count=0
+# while i <120:
+#     i+=2
+#     count+=1
+
+# print(i,count)
+
+# ---------------------------------------------------------------------------------
+
+
+# i=0
+# while i<10:
+#     if i==5:
+#         i+=1
+#         continue
+#     print(i)
+#     i+=1
+
+# ---------------------------------------------------------------------------------
+# product
+
+# num=int(input("enter a number: "))
+# i=0
+# while i<10:
+
+#     i+=1
+#     prod=num*i
+#     # print(i)
+#     print(i,"*",num,"=",prod)
+
+
+
+# ---------------------------------------------------------------------------------
+# prime number
+
+# num=int(input("enter a number: "))
+# i=0
+# while i<1:
+
+#     i+=1
+#     prime=num%2==0 and num//num==0
+#     # print(i)
+#     print(prime)
+
+# ---------------------------------------------------------------------------------
+sum=0
+L=[2,6,5,8,9,3]
+for n in L:
+    sum+=n
+print(sum)
+avg=sum/n(L)
+
+
+print(avg)
+    
+# ---------------------------------------------------------------------------------
+
+
+# avg=sum/n(L)
+
+    
+    
+# print(avg)
